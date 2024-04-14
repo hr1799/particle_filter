44cfaf28d093e9adf7ec4bb597f90c079e7776f3
Hariharan Ravichandran <hariharanr1799@gmail.com>
2024-04-13 21:43:41 -0400

    Modified code to ROS 2. Built test successful

--- setup.py
+++ setup.py
@@ -1,7 +1,31 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-d = generate_distutils_setup(
-    packages=['utils'],
-    package_dir={'': 'src'}
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'particle_filter'
+
+setup(
+    name=package_name,
+    version='0.2.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Hariharan Ravichandran',
+    maintainer_email='hariharanr1799@gmail.com',
+    description='Particle Filter Localization using RangeLibc for accelerated ray casting.',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'particle_filter = particle_filter.particle_filter:main'
+        ],
+    },
 )
-setup(**d)
